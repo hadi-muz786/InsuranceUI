1f99d9efc4703c999665c258c8f847db9bbaf0f8
abdul hadi <hadi286@icloud.com>
2020-01-21 15:40:00 -0500

    first angular project adding

---
+++ src/app/CustomerDto.ts
@@ -0,0 +1,13 @@
+export class CustomerDto {
+
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public email: string,
+        public phone: number
+
+    )
+    {
+
+    }
+}
